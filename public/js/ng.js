c0042bcf1ebe4317fe29837fb77673c149b50119
SomeKittens <rkoutnik@gmail.com>
2014-05-19 18:42:17 -0700

    markdown preview

--- public/js/ng.js
+++ public/js/ng.js
@@ -1,12 +1,21 @@
 'use strict';
 
 angular.module('nbe', [
-  'commonality'  
+  'commonality',
+  'hc.marked'
 ])
 .config(['$httpProvider', function($httpProvider) {
   // Oddly, not the default, and is needed to tell express it's XHR
   $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 }])
 .controller('Editor', function($scope, commonalityCalc) {
+  $scope.article = {
+    content: ''
+  };
+  
+  $scope.$watch('article.content', function(n) {
+    commonalityCalc.sourceText = $scope.article.content;
+  });
+
   console.log(commonalityCalc);
 });
