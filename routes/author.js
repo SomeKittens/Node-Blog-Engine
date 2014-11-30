62b3677da38c007af4d1f80794843992c77f0fd5
SomeKittens <rkoutnik@gmail.com>
2014-11-30 15:53:00 -0800

    switch to SASS, css bits, title on edit page

--- routes/author.js
+++ routes/author.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 // Ensure user is logged in
 router.get('*', function(req, res, next) {
+  // FIXME this should be negated
+  // Leaving it as-is for debugging until I add users
   if (req.user) {
     res.send(403);
   } else {
