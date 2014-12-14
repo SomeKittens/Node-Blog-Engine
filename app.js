940152459f7c0e94c641f03df1b197aa280b2647
SomeKittens <rkoutnik@gmail.com>
2014-12-13 17:24:34 -0800

    removed favicon module

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express')
   , path = require('path')
-  , favicon = require('static-favicon')
   , logger = require('morgan')
   , cookieParser = require('cookie-parser')
   , bodyParser = require('body-parser')
@@ -33,7 +32,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
